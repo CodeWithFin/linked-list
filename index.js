589c282d8d0fa92a2f18314503eaabaf0ed55028
Finley <Finley.mwachia12@gmail.com>
2025-02-12 10:36:23 +0300

    add insert method to linked list class

--- index.js
+++ index.js
@@ -1,66 +1,84 @@
-class Node{
-    constructor(value){
-        this.value = value
-        this.head = null
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.head = null;
+  }
 }
 
-class LinkedList{
-    constructor(){
-        this.head = null
-        this.size = 0
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  getSize() {
+    return this.size;
+  }
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
-    isEmpty(){
-        return this.size ===0
+    this.size++;
+  }
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is Empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
     }
-    getSize(){
-        return this.size
+  }
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
     }
-    prepend(value){
-        const node = new Node(value);
-        if(this.isEmpty()){
-            this.head = node
-        }else{
-            node.next = this.head
-            this.head = node
-        }
-        this.size++
+    this.size++;
+  }
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      return;
     }
-    print(){
-        if(this.isEmpty()){
-            console.log("List is Empty")
-        }else{
-            let curr = this.head
-            let listValues = ""
-            while(curr){
-                listValues += `${curr.value} `
-                curr = curr.next
-            }
-            console.log(listValues)
-        }
-    }
-    append(value){
+    if(index===0){
+        this.prepend(value)
+    }else{
         const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = node
-        }else{
-            let prev = this.head
-            while(prev.next){
-                prev = prev.next
-            }
-            prev.next = node
+        let prev = this.head
+        for(let i =0;i<index-1;i++){
+            prev = prev.next
         }
+        node.next = prev.next
+        prev.next = node
         this.size++
     }
+  }
 }
 
 const list = new LinkedList();
-console.log("Is List empty?",list.isEmpty())
-console.log("List size",list.getSize())
-list.print()
-list.append(10)
+console.log("Is List empty?", list.isEmpty());
+console.log("List size", list.getSize());
 list.print();
-list.append(20)
-list.append(30)
-list.append(40)
+list.append(10);
+list.print();
+list.append(20);
+list.append(30);
+list.append(40);
+list.insert(25,2)
 list.print();
