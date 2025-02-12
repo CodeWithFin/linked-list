f499c174c5894b08ebe5c6864a59d9c9df72b3de
Finley <Finley.mwachia12@gmail.com>
2025-02-12 09:58:14 +0300

    made changes to prepend method

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class LinkedList{
         return this.size
     }
     prepend(value){
-        const node = new Node();
+        const node = new Node(value);
         if(this.isEmpty()){
             this.head = node
         }else{
@@ -26,11 +26,27 @@ class LinkedList{
         }
         this.size++
     }
+    print(){
+        if(this.isEmpty()){
+            console.log("List is Empty")
+        }else{
+            let curr = this.head
+            let listValues = ""
+            while(curr){
+                listValues += `${curr.value} `
+                curr = curr.next
+            }
+            console.log(listValues)
+        }
+    }
 }
 
 const list = new LinkedList();
 console.log("Is List empty?",list.isEmpty())
 console.log("List size",list.getSize())
+list.print()
 list.prepend(10)
+list.print();
 list.prepend(20)
 list.prepend(30)
+list.print();
